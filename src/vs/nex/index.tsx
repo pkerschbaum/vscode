bf86016b6a5362eeeeaeeb01c0165b0ef0cae8ad
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2020-12-25 19:48:02 +0100

    feat: when ModeService languages get registered, re-render
    to show changed icons

--- src/vs/nex/index.tsx
+++ src/vs/nex/index.tsx
@@ -13,16 +13,19 @@ import {
 	NexFileSystem as nexFileSystem,
 } from 'vs/nex/platform/logic/file-system';
 import { commonStyles } from 'vs/nex/ui/Common.styles';
+import { createLogger } from 'vs/nex/base/logger/logger';
 import { App } from 'vs/nex/ui/App';
 import { createTheme } from 'vs/nex/theme';
 import { ThemeProvider } from 'vs/nex/ThemeProvider';
 import { dispatch, store } from 'vs/nex/platform/store/store';
 import { actions as fileProviderActions } from 'vs/nex/platform/store/file-provider/file-provider.slice';
 import { ResourceScheme } from 'vs/nex/platform/file-types';
+import { useDebounce } from 'vs/nex/platform/store/util/hooks.util';
 import { ModeServiceProvider } from 'vs/nex/ui/ModeService.provider';
 import { ModelServiceProvider } from 'vs/nex/ui/ModelService.provider';
 import { NexFileSystemProvider } from 'vs/nex/ui/NexFileSystem.provider';
 
+const logger = createLogger('index');
 const theme = createTheme(enUS);
 
 export function createApp(
@@ -51,23 +54,64 @@ export function createApp(
 			// render app
 			render(
 				<React.StrictMode>
-					<ModeServiceProvider value={modeService}>
-						<ModelServiceProvider value={modelService}>
-							<NexFileSystemProvider value={fileSystem}>
-								<ThemeProvider theme={theme}>
-									<Provider store={store}>
-										<CssBaseline />
-										<Box css={commonStyles.fullHeight}>
-											<App />
-										</Box>
-									</Provider>
-								</ThemeProvider>
-							</NexFileSystemProvider>
-						</ModelServiceProvider>
-					</ModeServiceProvider>
+					<Root modeService={modeService} modelService={modelService} fileSystem={fileSystem} />
 				</React.StrictMode>,
 				targetContainer,
 			);
 		},
 	};
 }
+
+const Root: React.FC<{
+	modeService: IModeService;
+	modelService: IModelService;
+	fileSystem: nexFileSystem;
+}> = ({ modeService, modelService, fileSystem }) => {
+	/*
+	 * Nex uses VS Code to show file and folder icons. The languages registered in the "ModeService"
+	 * of VS Code influence how icons get displayed. Even after the first render and paint of Nex, languages
+	 * get registered in the ModeService. Every time a language change occurs in the ModeService, we
+	 * need to re-render the application in order to show changed icons. That's why we register a listener
+	 * for language change events of the mode service, and force a re-render if the listener gets triggered.
+	 */
+	const [renderCount, setRenderCount] = React.useState(0);
+	const debouncedRenderCount = useDebounce(renderCount, 500);
+
+	modeService.onLanguagesMaybeChanged(() => {
+		logger.info(
+			`modeService.onLanguagesMaybeChanged got triggered ` +
+				`--> scheduling re-rendering of entire react tree...`,
+		);
+		setRenderCount((oldVal) => oldVal + 1);
+	});
+
+	return (
+		<RenderOnCountChange renderCount={debouncedRenderCount}>
+			<ModeServiceProvider value={modeService}>
+				<ModelServiceProvider value={modelService}>
+					<NexFileSystemProvider value={fileSystem}>
+						<ThemeProvider theme={theme}>
+							<Provider store={store}>
+								<CssBaseline />
+								<Box css={commonStyles.fullHeight}>
+									<App />
+								</Box>
+							</Provider>
+						</ThemeProvider>
+					</NexFileSystemProvider>
+				</ModelServiceProvider>
+			</ModeServiceProvider>
+		</RenderOnCountChange>
+	);
+};
+
+const RenderOnCountChange: React.FC<{
+	renderCount: number;
+	children: React.ReactElement;
+}> = React.memo(
+	({ children }) => {
+		logger.info(`(re-)rendering entire react tree...`);
+		return children;
+	},
+	(prevProps, nextProps) => prevProps.renderCount === nextProps.renderCount,
+);
